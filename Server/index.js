08bf424f8d5fe30608e62fbf0b8f6ed9f1d06c01
Fernando Siller <fernando@Fernandos-MBP.home>
2025-03-30 21:05:02 -0500

    fixed server errors and add gitignore

--- Server/index.js
+++ Server/index.js
@@ -10,5 +10,5 @@ app.get('/', (req, res) => {
   res.send('API is running 🎉');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
