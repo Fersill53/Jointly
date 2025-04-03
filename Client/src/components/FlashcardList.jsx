5ad771664c756e9af436a73cbbf431091604c8b7
Fernando Siller <fernando@Fernandos-MBP.home.local>
2025-04-02 19:28:09 -0500

    client fixed and server fixed by connecting to mongodb

--- Client/src/components/FlashcardList.jsx
+++ Client/src/components/FlashcardList.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 
 import axios from 'axios';
 
-const Flashcardlist = () => {
+const FlashcardList = () => {
     const [flashcards, setFlashcards] = useState ([]);
 
     useEffect(() => {
